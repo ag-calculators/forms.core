65934078711817926b4eb2a431b2dbd6e08086ba
John Robinson <johnrob@alliancecom.net>
2018-10-29 06:23:38 -0500

    First commit

---
+++ src/index.ts
@@ -0,0 +1,29 @@
+export interface FormInputInterface {
+    bind(formData: any): Promise<any>;
+    reset(detail:any): Promise<void>;
+    validate(): Promise<boolean>;
+    setValue(value:any): Promise<void>
+}
+
+export interface FormStateInterface {
+    setState(key: string, value: any): Promise<void>
+    getState(key: string): Promise<any>
+    clearState(): Promise<void>
+}
+
+export interface FormRouterInterface {
+    route(url: string, routeFn?: () => HTMLElement): Promise<void>
+}
+
+export type CssClassMap = { [className: string]: boolean };
+
+export function readProp (target: any, property: string, d?: any): any {
+    try {
+        return property.split('.').reduce( (a, b) => {
+            return a[b];
+        }, target);
+    } catch (e) {
+        return d;
+    }
+}
+
